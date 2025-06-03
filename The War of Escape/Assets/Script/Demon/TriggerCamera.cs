62d39d8b65998476cd9a49df22a227f7f063dc6a
rommel1917 <240284@jc-21.jp>
2025-06-03 17:23:13 +0900

    Demon/そんなに進んでない。

--- The War of Escape/Assets/Script/Demon/TriggerCamera.cs
+++ The War of Escape/Assets/Script/Demon/TriggerCamera.cs
@@ -1,32 +1,38 @@
 using UnityEngine;
 
-public class PlayerCamera : MonoBehaviour
+public class TriggerCamera : MonoBehaviour
 {
     public GameObject targetObject;
+    private Player target;
     public bool demonHave;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        target = GetComponent<Player>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    /*private void OnTriggerStay(Collider other)
+    private void OnTriggerStay(Collider other)
     {
         if (other.gameObject == targetObject)
         {
             Debug.Log("Trigger");
 
-            if (demonHave)
+            if ((demonHave) && (Input.GetMouseButtonDown(0))) 
             {
-                if
+                //if()
+                {
+                    target.Speed *= 0.1f;
+                    Debug.Log("Stun");
+
+                }
             }
         }
-    }*/
+    }
 }
