207799e048af0e5ff928c405fe4508dd42dba63f
rui050816 <240100@jc-21.jp>
2025-06-20 13:05:20 +0900

    テレサがうまくいかん

---
+++ The War of Escape/Assets/Script/GhostItem.cs
@@ -0,0 +1,87 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostItem : Item
+{
+    [Header("Steal Settings")]
+    [SerializeField] private int stealAmount = 1;
+
+    [Header("Consolation Items")]
+    [SerializeField] private List<GameObject> consolationItems = new();
+
+    public override void Activate(Player user)
+    {
+        if (user == null || user.otherPlayer == null)
+        {
+            Debug.LogWarning("GhostItem: userまたはotherPlayerがnullです。");
+            return;
+        }
+
+        Player target = user.otherPlayer;
+
+        if (target.keyCount > 0)
+        {
+            int keysToSteal = stealAmount <= 0 ? target.keyCount
+                                               : Mathf.Min(stealAmount, target.keyCount);
+
+            for (int i = 0; i < keysToSteal; i++)
+            {
+                target.RemoveKey();
+                user.AddKey();
+            }
+
+            Debug.Log($"GhostItem: {keysToSteal} 鍵を Player{target.playerID} から Player{user.playerID} に奪いました。");
+        }
+        else
+        {
+            Debug.Log($"GhostItem: Player{target.playerID} は鍵を持っていません。代替アイテムを探します。");
+
+            if (consolationItems != null && consolationItems.Count > 0)
+            {
+                int index = Random.Range(0, consolationItems.Count);
+                GameObject selectedPrefab = consolationItems[index];
+
+                if (selectedPrefab == null)
+                {
+                    Debug.LogWarning("GhostItem: 選ばれたプレハブが null です！");
+                    return;
+                }
+
+                GameObject obj = Instantiate(selectedPrefab);
+                Debug.Log($"GhostItem: {selectedPrefab.name} を生成しました。");
+
+                if (obj.TryGetComponent<Item>(out Item newItem))
+                {
+                    if (user.HasItem)
+                    {
+                        Debug.Log($"GhostItem: 既存アイテム {user.heldItem.name} を削除します。");
+                        Destroy(user.heldItem.gameObject);
+                    }
+
+                    user.SetHeldItem(newItem);
+                    obj.transform.SetParent(user.transform);
+                    obj.transform.localPosition = Vector3.zero;
+
+                    var col = obj.GetComponent<Collider>();
+                    if (col) col.enabled = false;
+
+                    var mesh = obj.GetComponent<MeshRenderer>();
+                    if (mesh) mesh.enabled = false;
+
+                    Debug.Log($"GhostItem: {newItem.name} を Player{user.playerID} に渡しました。");
+                }
+                else
+                {
+                    Debug.LogWarning("GhostItem: インスタンスに Item スクリプトが付いていません！");
+                    Destroy(obj);
+                }
+            }
+            else
+            {
+                Debug.LogWarning("GhostItem: consolationItems が空です。何も渡せません。");
+            }
+        }
+
+        Destroy(gameObject);
+    }
+}
