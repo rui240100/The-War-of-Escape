99107f08093f6f7267ef0bb4da61aaa6bd660ba2
takatotoyosima <240678@jc-21.jp>
2025-07-03 18:26:58 +0900

    多分鬼のアニメーションできたかも

---
+++ The War of Escape/Assets/Script/TitleSceneManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitlesceneManager : MonoBehaviour
+{
+    [Header("遷移先シーン名")]
+    [SerializeField] private string nextSceneName = "MainScene"; // 遷移させたいシーン名を設定
+
+    void Update()
+    {
+        if (AnyButtonPressed())
+        {
+            SceneManager.LoadScene(nextSceneName);
+        }
+    }
+
+    // 何かしらのボタンが押されたか判定
+    bool AnyButtonPressed()
+    {
+        // Xboxコントローラー含め、登録されている全ボタンをチェック
+        for (int i = 0; i < 20; i++) // ボタン数は必要に応じて調整
+        {
+            if (Input.GetKeyDown("joystick button " + i))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
