9ee9ec8af10b7be1f4cc94de01ec37de0545689c
rommel1917 <240284@jc-21.jp>
2025-07-16 12:59:32 +0900

    アイテム終わり。後は文章設定のみ。

--- The War of Escape/Assets/Script/Item.cs
+++ The War of Escape/Assets/Script/Item.cs
@@ -6,10 +6,23 @@ public class Item : MonoBehaviour
     public float slowMultiplier = 0.2f;
 
     public Sprite icon;
+
+    private Player playerScript;
+    public string useMessageSl1 = "アイテムを使用しました";
+    public string useMessageSl2 = "アイテムが使用されました";
+    private GameObject itemUse;
+    private ItemUse itemUseSc;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // シーン内のMessageDisplayManagerを探す
+        itemUse = GameObject.Find("ItemUse");
+        itemUseSc = itemUse.GetComponent<ItemUse>();
+        if (itemUseSc == null)
+        {
+            Debug.LogError("MessageDisplayManagerがシーンにありません！");
+        }
     }
 
     // Update is called once per frame
@@ -53,6 +66,22 @@ public class Item : MonoBehaviour
         if (user.otherPlayer != null)
         {
             user.StartCoroutine(user.otherPlayer.SlowDown(slowMultiplier, slowDuration));
+        }
+
+        if (itemUseSc != null)
+        {
+            if (playerScript.playerID == 1)
+            {
+                string message1 = useMessageSl1;
+                string message2 = useMessageSl2;
+                itemUseSc.ShowMessage(message1, message2);
+            }
+            else if (playerScript.playerID == 2)
+            {
+                string message1 = useMessageSl2;
+                string message2 = useMessageSl1;
+                itemUseSc.ShowMessage(message1, message2);
+            }
         }
 
         Destroy(gameObject); // アイテムは使い捨て
