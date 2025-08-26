9ed413c13bb2d1b5179a72d895bfd032b4f4d269
takatotoyosima <240678@jc-21.jp>
2025-08-26 17:21:00 +0900

    モデル適用、タイトル、クリアシーンのボタン押下時の効果音できた（はず）

--- The War of Escape/Assets/Script/ClearSceneController.cs
+++ The War of Escape/Assets/Script/ClearSceneController.cs
@@ -21,14 +21,29 @@ public class ClearSceneController : MonoBehaviour
         }
     }
 
+
+    [Header("効果音設定")]
+    public AudioSource audioSource;   // 音を再生するAudioSource
+    public AudioClip clip;            // 再生する音
+
+    [System.Obsolete]
+
+
     // Update is called once per frame
     void Update()
     {
 
+
         // XboxパッドのBボタンは "JoystickButton1" に対応
         if (Input.GetKeyDown(KeyCode.JoystickButton1))
         {
-            
+            // 効果音を再生
+            if (audioSource != null && clip != null)
+            {
+                audioSource.PlayOneShot(clip);
+            }
+
+
             FadeManager.Instance.LoadScene("TitleScene", 2.0f);
         }
     }
--- The War of Escape/Assets/Script/TitleSceneManager.cs
+++ The War of Escape/Assets/Script/TitleSceneManager.cs
@@ -3,21 +3,23 @@ using UnityEngine.SceneManagement;
 
 public class TitlesceneManager : MonoBehaviour
 {
+    [Header("Œø‰Ê‰¹İ’è")]
+    public AudioSource audioSource;   // ‰¹‚ğÄ¶‚·‚éAudioSource
+    public AudioClip clip;            // Ä¶‚·‚é‰¹
+
     [System.Obsolete]
     void Update()
     {
-
         if (Input.GetKeyDown(KeyCode.JoystickButton1))
         {
+            // Œø‰Ê‰¹‚ğÄ¶
+            if (audioSource != null && clip != null)
+            {
+                audioSource.PlayOneShot(clip);
+            }
 
+            // ƒV[ƒ“‘JˆÚ
             FadeManager.Instance.LoadScene("Stage-SIN", 2.0f);
         }
-
-
-
-
-
     }
 }
-
-    
